9d553f96508544e7a5515ef735d5d9b6db147da0
Michael Perry Goodman <michaelperrygoodman@gmail.com>
2017-10-31 23:18:41 -0500

    add api key, add youtube search package, stub components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const API_KEY = 'AIzaSyBXx3vCIa_g9yC_9YFn9BVwxDu9ABeB7A0';
+
 const App = () => {
   return (
     <div>hello world</div>
