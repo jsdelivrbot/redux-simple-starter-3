b8f2eab0a176e63d9545f419453bda19f0acc63f
Michael Perry Goodman <michaelperrygoodman@gmail.com>
2017-11-07 10:17:56 -0600

    call YTSearch test, remove useless google maps ref

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyBXx3vCIa_g9yC_9YFn9BVwxDu9ABeB7A0';
 
+YTSearch({key: API_KEY, term: 'mesa boogie c+'}, function(data) {
+  console.log(data);
+});
+
 const App = () => {
   return (
     <div>
