80b73dbf67bbf907416ba19a6edabb8c51e27a5e
Michael Perry Goodman <michaelperrygoodman@gmail.com>
2017-11-07 10:08:50 -0600

    finish search bar, add YTSearch

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-  return (
-    <div className="input-wrap">
-      <input />
-    </div>
-  );
-};
+// const SearchBar = () => {
+//   return (
+//     <div className="input-wrap">
+//       <input />
+//     </div>
+//   );
+// };
+
+class SearchBar extends Component {
+  // Class based objects only
+  // Initialize state
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
+  render() {
+    // Controlled component
+    return (
+      <div className="input-wrap">
+        <input
+          value={this.state.term}
+          onChange={event => this.setState({ term: event.target.value })}
+        />
+        <p>Value: {this.state.term}</p>
+      </div>
+    );
+  }
+
+  // handleInputChange(event) {
+  //   console.log(event.target.value);
+  // }
+}
 
 export default SearchBar;
