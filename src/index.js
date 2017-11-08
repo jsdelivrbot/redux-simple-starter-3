771263aab510078084f1a5bb56b9eb97fbeaeda7
Michael Perry Goodman <michaelperrygoodman@gmail.com>
2017-11-07 20:50:40 -0600

    refactor to class

--- src/index.js
+++ src/index.js
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyBXx3vCIa_g9yC_9YFn9BVwxDu9ABeB7A0';
 
-YTSearch({key: API_KEY, term: 'mesa boogie c+'}, function(data) {
-  console.log(data);
-});
+class App extends Component {
+  constructor (props) {
+    super(props);
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: ''}, (data) => {
+      this.setState({ videos: data });
+    });
+  }
+
+  render () {
+    return (
+     <div>
+       <SearchBar />
+     </div>
+   );
+  }
 }
+
+// const App = () => {
+//   return (
+//     <div>
+//       <SearchBar />
+//     </div>
+//   );
+// }
+
+
 
 ReactDOM.render(<App />, document.querySelector('.container'));
